fd1bb771cbc99b4cc92d495aad23f9cd679b7f56
HetviMungra <162463815+HetviMungra@users.noreply.github.com>
2024-03-13 16:24:25 +0530

    Add files via upload

---
+++ control str-3/1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main (){
+    int a,b,c;
+    a=8;
+    b=30;
+    c=1;
+    (a<b)? (a<c)? printf("A is minimum") :printf("C is minimum") :(b<c)? printf("b is minimum:") : printf("c is minimum") ;
+
+}
