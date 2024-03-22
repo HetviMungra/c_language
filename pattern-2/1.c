6100ae094029bd3c3663602f586beb848f909980
HetviMungra <162463815+HetviMungra@users.noreply.github.com>
2024-03-22 17:05:15 +0530

    Add files via upload

---
+++ pattern-2/1.c
@@ -0,0 +1,24 @@
+// _ _ _ _ 1
+//       2 1
+//     3 2 1
+// _ 4 3 2 1
+// 5 4 3 2 1 
+#include<stdio.h>
+main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int k=4;k>=i;k--)
+           {
+             printf(" ");
+           }
+        
+        for(int j=i;j>=1;j--)
+            {
+                printf("%d",j);
+            }
+           
+            printf("\n");
+    }
+  
+}
---
+++ pattern-2/3.c
@@ -0,0 +1,24 @@
+//         5
+//       4 4
+//     3 3 3
+//   2 2 2 2
+// 1 1 1 1 1
+
+#include <stdio.h>
+main()
+{
+     for(int i=5;i>=1;i--)
+    {
+        for(int k=1;k<=i;k++)
+           {
+             printf(" ");
+           }
+        
+        for(int j=i;j<=5;j++)
+            {
+                printf("%d",i);
+            }
+           
+            printf("\n");
+    }
+}
